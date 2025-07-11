db9bf5322df89d41eacf55bfc89d54752d0444dc
akshay1623 <142401102+akshay1623@users.noreply.github.com>
2025-07-11 15:51:18 +0400

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
@@ -20,6 +19,34 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* OG meta tags (for WhatsApp, LinkedIn, etc.) */}
+        <meta property="og:title" content="Akshayakumar Portfolio" />
+        <meta
+          property="og:description"
+          content="Welcome to the portfolio of Akshay — IT Specialist & Web Developer."
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://akshayakumar-portfolio-nxtjs-22h6.vercel.app/og-image.png"
+        />
+        <meta
+          property="og:url"
+          content="https://akshayakumar-portfolio-nxtjs-22h6.vercel.app"
+        />
+
+        {/* Twitter preview card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Akshayakumar Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Welcome to the portfolio of Akshay — IT Specialist & Web Developer."
+        />
+        <meta
+          name="twitter:image"
+          content="https://akshayakumar-portfolio-nxtjs-22h6.vercel.app/og-image.png"
+        />
+
         <link rel="icon" href="/jsm-logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
